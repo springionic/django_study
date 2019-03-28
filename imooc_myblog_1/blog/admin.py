46f14f492293b93d3e943e0b91c4ff18e7669c2e
springionic <2746565701@qq.com>
2019-03-28 16:50:08 +0800

    慕课网one结束

--- imooc_myblog_1/blog/admin.py
+++ imooc_myblog_1/blog/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Article
 
 # Register your models here.
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'pub_time')
+    list_filter = ('pub_time',)
+
+admin.site.register(Article, ArticleAdmin)
