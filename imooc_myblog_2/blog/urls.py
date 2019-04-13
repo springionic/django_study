0e091a88d1e423a57452b48d51df0e7ebf9d50fa
lilei <springionic@outlook.com>
2019-04-13 23:48:34 +0800

    慕课网blog2

---
+++ imooc_myblog_2/blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('hello_world/', views.hello_world),
+    path('content/', views.article_content),
+    path('index/', views.get_index_page),
+    path('detail/<int:article_id>', views.get_detail_page),
+]
